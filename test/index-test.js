79a09b22e759f01b129a2fb79294fecf2322ccdc
Maksym Tymchyk <reilag69@gmail.com>
2018-02-13 19:22:05 +0200

    Some fixes

--- test/index-test.js
+++ test/index-test.js
@@ -1,14 +1,14 @@
 'use strict';
 const assert = require('chai').assert;
-describe('index.js', function () {
-  it('can be required', function () {
+describe('index.js', function() {
+  it('can be required', function( ) {
     require('../.');
   });
-  it('has init', function () {
+  it('has init', function( ){
     var res = require('../.');
     assert.ok(res.init);
   });
-  it('init on mainnet', function () {
+  it('init on mainnet', function(){
     var client = require('../.');
     assert.ok(client.init(null, 'mainnet'));
   });
