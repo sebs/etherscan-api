8cd305947429e1262d214e02dd16496973251660
Sebastian Schürmann <sebs@2xs.org>
2018-06-06 14:00:56 +0200

    Merge pull request #29 from mazikwyry/feature/add-tokentx-action
    Add tokentx action support to account module which allows getting a list of ERC20 tokes transfer events

--- packages/etherscan-api/lib/account.js
+++ packages/etherscan-api/lib/account.js
@@ -149,6 +149,43 @@ module.exports = function(getRequest, apiKey) {
         module, action, address, apiKey
       });
       return getRequest(query);
+    },
+     /**
+     * [BETA] Get a list of "ERC20 - Token Transfer Events" by Address
+     * @param {string} address - Account address
+     * @param {string} startblock - start looking here
+     * @param {string} endblock - end looking there
+     * @param {string} sort - Sort asc/desc
+     * @param {string} contractaddress - Address of ERC20 token contract (if not specified lists transfers for all tokens)
+     * @example
+     * var txlist = api.account.tokentx('0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae', '0x5F988D968cb76c34C87e6924Cc1Ef1dCd4dE75da', 1, 'latest', 'asc');
+     * @returns {Promise.<object>}
+     */
+    tokentx(address, contractaddress, startblock, endblock, sort) {
+      const module = 'account';
+      const action = 'tokentx';
+
+      if (!startblock) {
+        startblock = 0;
+      }
+
+      if (!endblock) {
+        endblock = 'latest';
+      }
+
+      if (!sort) {
+        sort = 'asc';
+      }
+
+      var queryObject = {
+        module, action, startblock, endblock, sort, address, apiKey
+      };
+
+      if (contractaddress) {
+        queryObject.contractaddress = contractaddress;
+      }
+
+      return getRequest(querystring.stringify(queryObject));
     }
   };
 };
