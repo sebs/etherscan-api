f94c425eb5a9c1126c0d5d45522b31ae168f5daa
Sebastian Schürmann <sebs@2xs.org>
2018-06-15 12:24:29 +0200

    Added chart

--- packages/test-app/src/test-app-app/test-app-app.js
+++ packages/test-app/src/test-app-app/test-app-app.js
@@ -3,6 +3,8 @@ import 'etherscan-api/dist/bundle.js';
 import '@polymer/paper-input/paper-input.js';
 import '@polymer/paper-item/paper-item.js';
 import '@polymer/paper-listbox/paper-listbox.js';
+import '@webcomponents/shadycss/apply-shim.min.js';
+import 'chartjs-web-components/src/base.js'
 /**
  * @customElement
  * @polymer
@@ -14,8 +16,16 @@ class TestAppApp extends PolymerElement {
         :host {
           display: block;
         }
+
+        base-chart {
+      
+          width: 300px;
+          height: 300px;
+        }
       </style>
       <h1>[[name]] SUPPLY [[supply]]</h1>
+      <base-chart id="chart" type="bar" data="{{chartData}}" options="{{chartOptions}}"></base-chart>
+
       <paper-listbox selected="{{selectedToken}}">
         <template is="dom-repeat" items="{{tokens}}">
           <paper-item value="{{item.address}}">{{item.name}}</paper-item>
@@ -40,8 +50,36 @@ class TestAppApp extends PolymerElement {
 
   _changeToken(token) {
     this._triggerTokenSupply(this.tokens[token])
+    this._triggerHistory(this.tokens[token]);
   }
-
+  _triggerHistory(token) {
+    var me = this;
+    fetch('http://coincap.io/history/1day/'+token.name)
+      .then(function(response) {
+        return response.json();
+      })
+      .then((data)=>{
+        return data.price
+      })
+      .then(function(data) {
+        var labels = data.map((row, index)=>{
+          return index;
+        });
+        var data = data.map((row, index)=>{
+          return row[1];
+        });
+        
+        var data = {
+          labels: labels,
+          datasets: [
+              {
+                  data: data
+              }]
+      };
+        me.chartData = data;
+      });
+  };
+  
   _triggerBalance(address) {
    
     if (address.detail) {
@@ -72,17 +110,17 @@ class TestAppApp extends PolymerElement {
             name: 'EOS'
           }, {
             address: '0xf230b790e05390fc8295f4d3f60332c93bed42e2', 
-            name: 'Tronix'
+            name: 'TRX'
           }, {
             address: '0xB8c77482e45F1F44dE1745F52C74426C631bDD52', 
             name: 'BNB'
           }, {
             address: '0xd850942ef8811f2a866692a623011bde52a462c1', 
-            name: 'VeChain'
+            name: 'VEN'
           },
           {
             address: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07', 
-            name: 'OmiseGO'
+            name: 'OMG'
           }
 
         ]
@@ -93,6 +131,20 @@ class TestAppApp extends PolymerElement {
         observer: '_changeToken', 
         value: 0
       }, 
+      chartData: {
+        type: Object, 
+        notify: true, 
+        value: ()=>{
+          return {}
+        }
+      },
+      chartOptions: {
+        type: Object, 
+        notify: true, 
+        value: ()=>{
+          return {}
+        }
+      },
       supply: {
         type: Number, 
         notify: true
