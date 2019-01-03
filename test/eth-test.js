20e3526e915d963c963d4f5700e7577afb73603c
Sebastian Schürmann <sebs@2xs.org>
2019-01-03 10:30:01 +0100

    Improved tests

--- test/eth-test.js
+++ test/eth-test.js
@@ -2,7 +2,7 @@
 const assert = require('chai').assert;
 const init = require('../.').init;
 describe('proxy', function() {
-  xit('eth.getminedblocks', function(done){
+  it('eth.getminedblocks', function(done){
     var api = init();
     var txlist = api.account.getminedblocks('0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b');
     txlist.then(function(res){
