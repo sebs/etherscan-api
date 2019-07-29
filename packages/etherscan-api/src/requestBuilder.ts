26ca47d0b0931c0782094292bb6499efee71ca61
Sebastian Schürmann <sebs@2xs.org>
2019-07-29 08:10:23 +0200

    Add simple API doc generation

--- packages/etherscan-api/src/requestBuilder.ts
+++ packages/etherscan-api/src/requestBuilder.ts
@@ -1,6 +1,12 @@
 
 const base =  'https://api.etherscan.io/api'
+import { modules } from './modules'
 
-export const requestBuilder =  function(module:string, action:string):string {
+export const requestBuilder =  function(module: string, action: string): string {
+
+	if (!modules.get(module)) {
+		throw Error('unknown module')
+	}
+
 	return `${base}?module=${module}&action=${action}`
 }
