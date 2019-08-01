0f5fe5eaabeb430fca0f7dfb885e11823ac6421c
Sebastian Schürmann <sebs@2xs.org>
2019-08-01 13:43:35 +0200

    Starting to introduce entities

---
+++ src/entities/Apikey.ts
@@ -0,0 +1,17 @@
+
+import { Entity } from '../interfaces/Entity'
+
+export class ApiKey  implements Entity {
+	constructor(apiKey: string) {
+
+	}
+
+	validate(): void {
+		
+	}
+
+
+	valid(): boolean {
+		return true
+	}
+}
