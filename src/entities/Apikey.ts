c9276a6bdbd538cfc7de12ee75d750a8a79be338
Sebastian Schürmann <sebs@2xs.org>
2019-08-01 13:51:58 +0200

    ApiKey entity

--- src/entities/Apikey.ts
+++ src/entities/Apikey.ts
@@ -1,17 +1,20 @@
 
 import { Entity } from '../interfaces/Entity'
 
-export class ApiKey  implements Entity {
+export class ApiKey implements Entity {
 	private apiKey
+	private keyLength: number = 33
 	constructor(apiKey: string) {
 		this.apiKey = apiKey
 	}
 
 	validate(): void {
-		
+		if (!this.valid()) {
+			throw new Error('invalid')
+		}
 	}
 
 	valid(): boolean {
-		return this.apiKey.length !== 33
+		return this.apiKey.length !== this.keyLength
 	}
 }
