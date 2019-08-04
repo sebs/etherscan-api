c1eb0d90b465e1c76ddd23df204f6cec686a2bf4
Sebastian Schürmann <sebs@2xs.org>
2019-08-04 09:25:03 +0200

    Operands

--- packages/etherscan-api/test/parametersTest.ts
+++ packages/etherscan-api/test/parametersTest.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 import { sort } from '../src/parameters/sort'
-
+import { operand } from '../src/parameters/operand'
 
 test('sort asc exists', t => {
 	t.truthy(sort.asc)
@@ -10,4 +10,10 @@ test('sort asc can be compared to a string', t => {
 	t.is(sort.asc, 'asc')
 })
 
+test('operand and', t => {
+	t.is(operand.and, 'and')
+})
 
+test('operand or', t => {
+	t.is(operand.or, 'or')
+})
