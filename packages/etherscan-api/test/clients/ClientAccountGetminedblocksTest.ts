2f9aca4ef413c7ca80bba19d78ea4dac5a35519d
Sebastian Schürmann <sebs@2xs.org>
2019-08-31 13:35:08 +0200

    ci: added simple travis configuration
    * install lerna in pre step

--- packages/etherscan-api/test/clients/ClientAccountGetminedblocksTest.ts
+++ packages/etherscan-api/test/clients/ClientAccountGetminedblocksTest.ts
@@ -14,9 +14,7 @@ const type = 'block'
 const address = new Address('0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b')
 const tag = 'latest'
 const apiKey = new ApiKey('TRU5Z5MNWIEYP4F6DPH2T53IJWZIZ5GT1W')
-
 const expectedUrl = 'https://api.etherscan.io/api?module=account&action=getminedblocks&address=0x9dd134d14d1e65f84b706d6f205cd5b1cd03a46b&blocktype=blocks&page=1&offset=10&apikey=TRU5Z5MNWIEYP4F6DPH2T53IJWZIZ5GT1W'
-
 
 test('exists', t => {
 	t.truthy(ClientAccountGetminedblocks)
@@ -28,7 +26,7 @@ test('can be instantiated', t => {
 })
 
 test('generates the right url', t => {
-	const c = new ClientAccountGetminedblocks(apiKey, 'account', 'balancemulti', address, tag)
+	const c = new ClientAccountGetminedblocks(apiKey, 'account', 'getminedblocks', address, tag)
 	const url = c.toUrl()
 	const parsedUrl = parse(url, true).query
 	const parsedExpectedUrl = parse(expectedUrl, true).query
